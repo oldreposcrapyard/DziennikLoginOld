c1ff7f9b24edffd5816dcd8cae604c36d12a4902
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-05-05 21:43:15 +0200

    Optimalization all the way!
    Cut the execution time by 2 seconds!

--- tableExtractor.php
+++ tableExtractor.php
@@ -4,41 +4,64 @@ error_reporting(E_ALL);
 //include library
 require 'simple_html_dom.php';
 
-//policz przedmioty
-function countSubjects($file){//takes document as argument
+// policzyc ilosc komorek i sprawdzic ktore sa z ocenami, tylko te pokazac
+/*function getGrades($file,$rowNumber){//takes document as argument and row number (0 is title row)
 $html = file_get_html($file);
+if(!empty($html->find('table',4)->find('tr',$rowNumber)->find('span',0)->plaintext)){//$html->find('ul', 0)->find('li', 0);
+$row = $html->find('table',4)->find('tr',$rowNumber)->find('span',0)->plaintext;
+return $row;
+}
+else{
+return ' BRAK';
+}
+
+
+$html->clear();
+unset($html);
+}
+*/
+
+
+//Define encoding
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+//FILE!
+$file='EXAMPLEDATA.html';
+//Lets get something to eat!
+$html = file_get_html($file);
+//Count the subjects:
 $rowsEven = $html->find('table',4)->find('.data_even');
 $countEven = count($rowsEven);
 $rowsOdd = $html->find('table',4)->find('.data_odd');
 $countOdd = count($rowsOdd);
-$count = $countOdd + $countEven;
-return $count;
-$html->clear();
-unset($html);
-}
-//nazwa przedmiotu
-function getSubjectName($file,$rowNumber){//takes document as argument and row number (0 is title row)
-$html = file_get_html($file);
-$row = $html->find('table',4)->find('tr',$rowNumber)->find('td',0);//$html->find('ul', 0)->find('li', 0);
-return $row;
-$html->clear();
-unset($html);
-}
+$subjectsCount = $countOdd + $countEven; //18
 
-//Define encoding
-echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
-//PLIK
-$file='EXAMPLEDATA.html';
-$subjectsCount = countSubjects($file);//policz przedmioty - 18
-echo "ILOSC PRZEDMIOTOW:$subjectsCount<br>";//wyswietl
+echo "ILOSC PRZEDMIOTOW:$subjectsCount<br>";//Echo, echo :p
 
-//list subjects
+//Here We Go!
 $i=0;
 while ($i < $subjectsCount) {
-    $i++;//(0 is title row)
-    $subjectName=getSubjectName($file,$i);
-    echo "SUBJECTNAME:$subjectName";
+    $i++;//(0 is title row)   
+    //The number:
+    echo 'NUMBER:'.$i.' ';
+    //Hey, the subjectname:
+    $subjectName = $html->find('table',4)->find('tr',$i)->find('td',0)->plaintext;//$html->find('ul', 0)->find('li', 0);
+    echo "SUBJECTNAME:$subjectName ";
+    // The average ones get to eat too:
+    if(!empty($html->find('table',4)->find('tr',$i)->find('.cell-style-srednia',0)->plaintext)){
+    $average = $html->find('table',4)->find('tr',$i)->find('.cell-style-srednia',0)->plaintext;
+    }
+    else{
+    //No food for ya!
+    $average = ' BRAK';
+    }
+    echo "AVERAGE:$average".'<br>';
+    //$subjectGrades=getGrades($file,$i);
+    //echo "GRADES:$subjectGrades ";
+
 }
+//Clean up after ourselves ;)
+$html->clear();
+unset($html);
 
 //TIME
 $time_end = microtime(true);
