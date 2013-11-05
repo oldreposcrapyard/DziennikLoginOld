9eda0cce215ff823ebaeaf4605a70985f82137b4
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-11-05 16:11:04 +0100

    added graph class

---
+++ DziennikLogin/classes/graphReportGenerator.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'reportGenerator.php';
+include('pChart/class/pData.class.php');
+include('pChart/class/pDraw.class.php');
+include('pChart/class/pImage.class.php');
+
+/**
+ * Description of graphReportGenerator
+ * 
+ * This report generator makes statistical data for each user into grapgical form.
+ * Some examples include the amount of each grade, school-wide grade statistics
+ * and średnia ocen w danej wadze/w danym typie oceny
+ *
+ * @author marcin
+ */
+class graphReportGenerator extends reportGenerator{
+    //put your code here
+}
+
+?>
