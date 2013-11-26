94ef602bd047e3f5e1b9a4610b5bca6d285d9c55
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-11-26 16:08:59 +0100

    Fixed palette

--- DziennikLogin/classes/pChart/chartGenerator.php
+++ DziennikLogin/classes/pChart/chartGenerator.php
@@ -113,18 +113,18 @@ class graphReportGenerator {
         $myPicture->setShadow(TRUE, array("X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10));
         /* Draw the scale, keep everything automatic */
         $myPicture->drawScale();
-        /* Create the per bar palette */ //as per http://ideone.com/vWSh6o
-        $Palette = array("0" => array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 70),
-            "1" => array("R" => 230, "G" => 25, "B" => 0, "Alpha" => 70),
-            "2" => array("R" => 205, "G" => 50, "B" => 0, "Alpha" => 70),
-            "3" => array("R" => 180, "G" => 75, "B" => 0, "Alpha" => 70),
-            "4" => array("R" => 155, "G" => 100, "B" => 0, "Alpha" => 70),
-            "5" => array("R" => 130, "G" => 125, "B" => 0, "Alpha" => 70),
-            "6" => array("R" => 105, "G" => 150, "B" => 0, "Alpha" => 70),
-            "7" => array("R" => 80, "G" => 175, "B" => 0, "Alpha" => 70),
-            "8" => array("R" => 55, "G" => 200, "B" => 0, "Alpha" => 70),
-            "9" => array("R" => 30, "G" => 225, "B" => 0, "Alpha" => 70),
-            "10" => array("R" => 0, "G" =>255, "B" => 0, "Alpha" => 70)
+        /* Create the per bar palette */ //as per http://ideone.com/vWSh6o nope
++        $Palette = array("0" => array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 100),//FF0000 don
+ +            "1" => array("R" => 255, "G" => 51, "B" => 0, "Alpha" => 100),//FF3300 don
+ +            "2" => array("R" => 255, "G" => 102, "B" => 0, "Alpha" => 100),//FF6600 don
+ +            "3" => array("R" => 255, "G" => 153, "B" => 0, "Alpha" => 100),//FF9900 don
+ +            "4" => array("R" => 255, "G" => 204, "B" => 0, "Alpha" => 100),//FFCC00 don
+ +            "5" => array("R" => 255, "G" => 255, "B" => 0, "Alpha" => 100),//FFFF00 don
+ +            "6" => array("R" => 204, "G" => 255, "B" => 0, "Alpha" => 100),//CCFF00 don
+ +            "7" => array("R" => 153, "G" => 255, "B" => 0, "Alpha" => 100),//99FF00 don
+ +            "8" => array("R" => 102, "G" => 255, "B" => 0, "Alpha" => 100),//66FF00 don
+ +            "9" => array("R" => 51, "G" => 255, "B" => 0, "Alpha" => 100),//33FF00 don
+ +            "10" => array("R" => 0, "G" =>255, "B" => 0, "Alpha" => 100)//00FF00 don
             );
 
         /* Draw the scale, keep everything automatic */
