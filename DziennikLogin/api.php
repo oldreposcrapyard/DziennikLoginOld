0d2afeb669499dd0fe1ad994f0393763c87a695a
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-11-26 16:59:09 +0100

    updated

--- DziennikLogin/api.php
+++ DziennikLogin/api.php
@@ -1,5 +1,17 @@
 <?php
 
 //begin work on api for mobile and desktop apps
+//end of class
+require 'config.local.php';
 
+$db_host = $CONF['databaseHost'];
+$db_name = $CONF['databaseName'];
+$db_username = $CONF['databaseUsername'];
+$db_password = $CONF['databasePassword'];
+
+$chartGenerator = new graphReportGenerator();
+
+$chartGenerator->setDatabaseConnectionData($db_host, $db_name, $db_username, $db_password);
+$chartGenerator->setUserId(2);
+$chartGenerator->executeProcessing();
 ?>
