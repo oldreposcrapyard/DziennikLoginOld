4ad0f9bc8415031d1db5490eebe4e9cb3715cc21
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-02-21 21:28:07 +0100

    Test for logfiles, first part of new status tool

---
+++ DziennikLogin/tools/status.php
@@ -0,0 +1,16 @@
+<?php
+require '../classes/Monolog/Logger.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+
+// Create the logger
+$logger = new Logger('DziennikLoginLogger');
+// Now add some handlers
+$logger->pushHandler(new StreamHandler('../logs/error.log', Logger::DEBUG));
+$logger->pushHandler(new FirePHPHandler());
+
+// You can now use your logger
+$logger->addInfo('My logger is now ready');
+
+?>
