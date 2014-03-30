7fae73b9904732dc0911c09fc8124bb99dd75fef
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-03-30 23:54:58 +0200

    added an isActive switch to report jobs

--- DziennikLogin/cron/dailyReportSender.php
+++ DziennikLogin/cron/dailyReportSender.php
@@ -21,7 +21,7 @@ try {
 }
 
 try {
-    $stmt = $pdo->query('SELECT * FROM reportjobs WHERE reportType = \'DAILY\'');
+    $stmt = $pdo->query('SELECT * FROM reportjobs WHERE reportType = \'DAILY\' AND isActive=1');
     $stmt->execute();
     $jobsArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
